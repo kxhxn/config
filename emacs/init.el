260ecfc55324f212bc40252d2e900864526345f9
kxhxn <hilarykodji@gmail.com>
2024-08-19 11:51:19 +0000

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
-;; perf settings
+					; perf settings
 ;; Minimize garbage collection during startup
 (setq gc-cons-threshold most-positive-fixnum)
 ;; Lower threshold back to 8 MiB (default is 800kB)
@@ -31,7 +31,8 @@
   (global-hl-line-mode t)
   (global-display-line-numbers-mode t)
   (electric-pair-mode t)
-  (global-set-key (kbd "C-x C-b") 'ibuffer))
+  (global-set-key (kbd "C-x C-b") 'ibuffer)
+  (delete-selection-mode t))
 
 (use-package doom-themes
   :straight t)
@@ -73,7 +74,8 @@
 
 (use-package eglot
   :straight nil
-  :config (add-to-list 'eglot-server-programs '(d-mode . ("serve-d"))))
+  :config (add-to-list 'eglot-server-programs '(d-mode . ("serve-d")))
+  (add-to-list 'eglot-server-programs '(move-mode "move-analyzer")))
 
 (use-package company
   :straight t
@@ -86,10 +88,10 @@
 (use-package vertico
   :straight t
   :custom
-  ;; (vertico-scroll-margin 0) ;; Different scroll margin
-  ;; (vertico-count 20) ;; Show more candidates
+  (vertico-scroll-margin 0) ;; Different scroll margin
+  (vertico-count 20) ;; Show more candidates
   (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
-  ;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
   :init
   (vertico-mode))
 
@@ -107,14 +109,31 @@
   :straight t
   :init (exec-path-from-shell-initialize))
 
+(use-package orderless
+  :straight t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package treesit-auto
+  :straight t
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
+;; (use-package ws-butler
+;;   :straight t
+;;   :hook (prog-mode . ws-butler-mode))
+
 ;; Languages
 (use-package geiser-mit
   :straight t)
 
 (use-package move-mode
   :straight t
-  ;; :hook (move-mode . eglot-ensure)
-  )
+  :hook (move-mode . eglot-ensure))
 
 (use-package markdown-mode
   :straight t
@@ -122,12 +141,16 @@
 
 (use-package csproj-mode
   :straight t)
-
 (use-package sharper
   :straight t
   :bind
   ("C-c n" . sharper-main-transient)
   :config (setq sharper-run-only-one t))
+;; (use-package dotnet
+;;   :straight t
+;;   :hook (csharp-mode . dotnet-mode)
+;;   :bind-keymap
+;;   ("C-c n" . dotnet-mode-command-map))
 (use-package csharp-mode
   :straight nil
   :hook
