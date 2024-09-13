2ba027a91cb77b8c764482aad8d1e6b3ac7ff138
kxhxn <hilarykodji@gmail.com>
2024-09-13 11:44:36 +0000

    Update early-init.el

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,1 +1,12 @@
+(setq gc-cons-threshold 1000000000)
+(run-with-idle-timer
+ 5 nil
+ (lambda ()
+   (setq-default gc-cons-threshold (* 1024 1024 100))
+   ;; (message "gc-cons-threshold restored to %S" gc-cons-threshold)
+   ))
+
+(setq-default lexical-binding t
+              load-prefer-newer t)
+
 (setq package-enable-at-startup nil)
